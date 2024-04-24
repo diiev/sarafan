363b3a7b304e260d69b9b1353019e48d9dcad256
diiev <ramzans096@gmail.com>
2024-04-24 13:31:54 +0300

    .

--- src/assets/js/modules/menu.js
+++ src/assets/js/modules/menu.js
@@ -1,11 +1,25 @@
 const menu = () => {
-
+    let count = 0
     const burger = document.querySelector('.burger');
-    const menu = document.querySelector('.menu');
+    const menu = document.querySelector('.menu'); 
+   
     burger.addEventListener('click', () => {
         burger.classList.toggle('burger__active');
         menu.classList.toggle('menu__active');
         menu.classList.toggle('fade');
+        document.body.classList.toggle('active')
+        // count++;
+        // document.body.style.paddingRight = '17px'
+        // document.body.style.overflow = 'hidden';
+        
+        // if (count > 1)  {
+        //     count = 0;
+        //     document.body.style.paddingRight = '0px'
+        //     document.body.style.overflow = 'scroll';
+            
+        // }
+         
+
        
 
     });
