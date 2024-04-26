53f55e713237c825639743da111edbb64c0c0f1e
diiev <ramzans096@gmail.com>
2024-04-26 11:48:07 +0300

    .

--- dist/inc_home-img.php
+++
@@ -1,1 +0,0 @@
-<img src="<?=SITE_TEMPLATE_PATH?>/assets/img/main_.jpg" alt="main">
--- dist/inc_logo.php
+++
@@ -1,3 +0,0 @@
-<a href="/" class="logo header__logo">
-              <img src="<?=SITE_TEMPLATE_PATH?>/assets/img/logo.png" alt="logo">
-            </a>
